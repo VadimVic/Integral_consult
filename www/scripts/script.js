e3cfd8bc29336786f030dcbbfe223ad94aeb9282
Vadim Bykov <nordvadim@bk.ru>
2021-09-08 14:14:16 +0300

    created services

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,16 @@
-console.log('script work!');
+$(document).ready(function () {
+
+$('.team-links a').on('click', function (e) {
+    e.preventDefault();
+
+    let index = $(this).index('.team-links a');
+    console.log(index);
+
+    $('.team-links a').removeClass('active');
+    $(this).addClass('active');
+
+    $('.team-info').removeClass('team-info-visible');
+    $('.team-info').eq(index).addClass('team-info-visible');
+  });
+
+});
