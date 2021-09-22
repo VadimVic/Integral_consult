faba482875a321283d77eb4b2f07a4f64573b983
Vadim Bykov <nordvadim@bk.ru>
2021-09-22 12:03:09 +0300

    refresh the slider

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -16,70 +16,132 @@ $(document).ready(function () {
   
     //slider
   
-    $(function() {
+    // $(function() {
           
-      var Page = (function() {
+    //   var Page = (function() {
   
-        var $navArrows = $( '#nav-arrows' ).hide(),
-          $navOptions = $( '#nav-options' ).hide(),
-          $shadow = $( '#shadow' ).hide(),
-          slicebox = $( '#sb-slider' ).slicebox( {
-            onReady : function() {
+    //     var $navArrows = $( '#nav-arrows' ).hide(),
+    //       $navOptions = $( '#nav-options' ).hide(),
+    //       $shadow = $( '#shadow' ).hide(),
+    //       slicebox = $( '#sb-slider' ).slicebox( {
+    //         onReady : function() {
   
   
-              $navArrows.show();
-              $navOptions.show();
-              $shadow.show();
+    //           $navArrows.show();
+    //           $navOptions.show();
+    //           $shadow.show();
   
-            },
-            orientation : 'h',
-            cuboidsCount : 3
-          } ),
+    //         },
+    //         orientation : 'h',
+    //         cuboidsCount : 10,
+    //         sequentialFactor : 100,
+    //         speed : 2000,
+    //         easing : 'ease',
+    //         autoplay : true,
+    //         interval: 8000,
+    //         fallbackFadeSpeed : 1300,
+    //       } ),
           
-          init = function() {
+    //       init = function() {
   
-            initEvents();
+    //         initEvents();
             
-          },
-          initEvents = function() {
+    //       },
+    //       initEvents = function() {
   
-            // add navigation events
-            $navArrows.children( ':first' ).on( 'click', function() {
+    //         // add navigation events
+    //         $navArrows.children( ':first' ).on( 'click', function() {
   
-              slicebox.next();
-              return false;
+    //           slicebox.next();
+    //           return false;
   
-            } );
+    //         } );
   
-            $navArrows.children( ':last' ).on( 'click', function() {
+    //         $navArrows.children( ':last' ).on( 'click', function() {
               
-              slicebox.previous();
-              return false;
+    //           slicebox.previous();
+    //           return false;
   
-            } );
+    //         } );
   
-            $( '#navPlay' ).on( 'click', function() {
+    //         $( '#navPlay' ).on( 'click', function() {
               
-              slicebox.play();
-              return false;
+    //           slicebox.play();
+    //           return false;
   
-            } );
+    //         } );
   
-            $( '#navPause' ).on( 'click', function() {
+    //         $( '#navPause' ).on( 'click', function() {
               
-              slicebox.pause();
-              return false;
+    //           slicebox.pause();
+    //           return false;
   
-            } );
+    //         } );
   
-          };
+    //       };
   
-          return { init : init };
+    //       return { init : init };
   
+    //   })();
+  
+    //   Page.init();
+  
+    // });
+
+
+
+    $(function() {
+				
+      var Page = (function() {
+        
+        var $navArrows = $( '#nav-arrows' ).hide(),
+            $shadow = $( '#shadow' ).hide(),
+            slicebox = $( '#sb-slider' ).slicebox( {
+              onReady : function() {
+                
+                $navArrows.show();
+                $shadow.show();
+                
+              },
+              orientation : 'r',
+              cuboidsRandom : true,
+              disperseFactor : 30
+            } ),
+            
+            init = function() {
+              
+              initEvents();
+              
+            },
+            initEvents = function() {
+              
+              // add navigation events
+              $navArrows.children( ':first' ).on( 'click', function() {
+                
+                slicebox.next();
+                return false;
+                
+              } );
+              
+              $navArrows.children( ':last' ).on( 'click', function() {
+                
+                slicebox.previous();
+                return false;
+                
+              } );
+              
+            };
+        
+        return { init : init };
+        
       })();
-  
+      
       Page.init();
-  
+      
     });
+
+
+
+
   
   });
